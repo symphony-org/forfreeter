fb1e814a1704b702eb80524c1c5fef30142fff88
Lukasz Golebiewski <lukasz.golebiewski@gmail.com>
2020-10-08 16:26:27 +0200

    Generate empty instance
    Use derive-topdown lib

--- test/InstanceSpec.hs
+++ test/InstanceSpec.hs
@@ -1,18 +1,29 @@
 module InstanceSpec where
 
+import           Forfreeter.Instance
 import           Hedgehog
-import           Hedgehog.Gen   as Gen
-import           Hedgehog.Range as Range
-import           Instance
+import           Hedgehog.Gen        as Gen
+import           Hedgehog.Range      as Range
 
 data User = User
 
+class SomeClass t where
+  someFun :: t -> String
+  default someFun :: Show t => t -> String
+  someFun t = "Hello from someFun! Got: " <> show t
+
 mkShow ''User
+mkEmptyInstance ''SomeClass ''()
 
 prop_1 :: Property
 prop_1 =
   property $ do
     ( show User ) === "hello from template show"
+
+prop_some_class :: Property
+prop_some_class =
+  property $ do
+    someFun () === "Hello from someFun! Got: ()"
 
 tests :: IO Bool
 tests =
