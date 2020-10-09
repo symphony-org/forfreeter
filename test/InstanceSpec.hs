1fa3984549075af75ee502bf43a80eac5f818441
Lukasz Golebiewski <lukasz.golebiewski@gmail.com>
2020-10-09 16:20:09 +0200

    Generate instance with a fun
    Without default impl

--- test/InstanceSpec.hs
+++ test/InstanceSpec.hs
@@ -6,14 +6,21 @@ import           Hedgehog.Gen        as Gen
 import           Hedgehog.Range      as Range
 
 data User = User
+data User2 = User2
 
 class SomeClass t where
   someFun :: t -> String
   default someFun :: Show t => t -> String
   someFun t = "Hello from someFun! Got: " <> show t
 
+class SomeClass2 t where
+  someFun2 :: t -> String
+
 mkShow ''User
 mkEmptyInstance ''SomeClass ''()
+mkEmptyInstance ''SomeClass ''User
+
+mkOverlappable ''SomeClass2 ''User2
 
 prop_1 :: Property
 prop_1 =
@@ -23,7 +30,12 @@ prop_1 =
 prop_some_class :: Property
 prop_some_class =
   property $ do
-    someFun () === "Hello from someFun! Got: ()"
+    someFun User === "Hello from someFun! Got: hello from template show"
+
+prop_some_class_2 :: Property
+prop_some_class_2 =
+  property $ do
+    someFun2 User2 === "Generated in overlappable"
 
 tests :: IO Bool
 tests =
