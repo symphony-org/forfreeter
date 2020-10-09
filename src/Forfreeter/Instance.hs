97fbb15adec23bb212e5f9508900a27193e8870b
Lukasz Golebiewski <lukasz.golebiewski@gmail.com>
2020-10-09 16:20:09 +0200

    Generate instance with a fun
    Without default impl

--- src/Forfreeter/Instance.hs
+++ src/Forfreeter/Instance.hs
@@ -4,11 +4,20 @@ import           Control.Monad.Trans.Class    (MonadTrans)
 import           Data.Derive.TopDown.Instance (instance_)
 import           Language.Haskell.TH
 
+class Test a where
+  foo :: a -> String
+
 mkEmptyInstance :: Name -> Name -> Q [Dec]
-mkEmptyInstance cName tName = instance_ cName tName
+mkEmptyInstance = instance_
 
 mkOverlappable :: Name -> Name -> Q [Dec]
-mkOverlappable _ _ = undefined
+mkOverlappable cName tName = do
+  [ InstanceD a b c d ] <- mkEmptyInstance cName tName
+  ClassI cDec@(ClassD _ n _ _ foos ) _ <- reify cName
+  [ sigD_@(SigD fName appT_) ] <- pure foos
+  exp <- [e| \_ -> "Generated in overlappable" |]
+  someFun <- pure $ FunD fName [ Clause [] (NormalB exp) []]
+  pure $ [ InstanceD a b c [someFun] ]
 
 mkShow :: Name -> Q [Dec]
 mkShow name = [d|
