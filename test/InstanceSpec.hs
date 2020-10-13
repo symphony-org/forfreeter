97b03120b3e4c44dd3555600a6f02836c6d5ca54
Pawel Szulc <paul.szulc@gmail.com>
2020-10-13 21:12:43 +0200

    Real world example (c) (#9)
    * Remove mkShow experiment
    
    * Change InstanceSpec with real world examples
    
    Nice thing is that now you can just compile forfreeter-test to check
    if templates are working.
    
    Additionally it is easily to build up more complex problems.
    
    This spec also nicely documents what forfreeter is doing
    
    * Add support for typeclasses with functions that have zero arguments
    
    * Add test showing that typeclasses with multiple functions are supported
    
    * Rename to mkOverlappable

--- test/InstanceSpec.hs
+++ test/InstanceSpec.hs
@@ -1,34 +1,84 @@
 {-# LANGUAGE UndecidableInstances #-}
 module InstanceSpec where
 
+import           Control.Monad.Trans.Class    (MonadTrans, lift)
+import           Control.Monad.Trans.Reader (ReaderT(..), ask)
+import           Control.Monad.Trans.Identity (IdentityT (..))
+import           Data.Function                ((&))
 import           Forfreeter.Instance
-import           Hedgehog            hiding (Test)
-import           Hedgehog.Gen        as Gen
-import           Hedgehog.Range      as Range
+import           Hedgehog                     hiding (Test)
+import           Hedgehog.Gen                 as Gen
+import           Hedgehog.Range               as Range
+import           Language.Haskell.TH
 
-data User = User
+---- Time
 
-class Test m where
-  foo :: a -> m String
+class Monad m => Time m where
+  currentTime :: m Int
 
-instance Test IO where
-  foo _ = pure "Hi from Test.foo"
+mkOverlappable ''Time
 
-mkShow ''User
+newtype ConstTime m a = ConstTime
+  { unConstTime :: ReaderT Int m a
+  }
+  deriving newtype (Functor, Applicative, Monad, MonadTrans)
 
-mkEmptyInstance ''Test
+runConstTime :: Int -> ConstTime m a -> m a
+runConstTime time = flip runReaderT time . unConstTime
 
-prop_1 :: Property
-prop_1 =
-  property $ do
-    ( show User ) === "hello from template show"
+instance Monad m => Time (ConstTime m) where
+  currentTime = ConstTime ask
 
-prop_some_class :: Property
-prop_some_class =
-  property $ do
-    res <- foo ()
-    res === "Hi from Test.foo"
+---- Queue
+
+class Monad m => Queue m where
+  send :: a -> m ()
+
+mkOverlappable ''Queue
+
+newtype NoOpQueue m a = NoOpQueue
+  { runNoOpQueue :: m a
+  }
+  deriving newtype (Functor, Applicative, Monad)
+  deriving MonadTrans via IdentityT
+
+instance Monad m => Queue (NoOpQueue m) where
+  send _ = pure ()
+
+---- Repository
+
+class Monad m => Repository m where
+  save :: a -> m ()
+  deleteAll :: m ()
+
+mkOverlappable ''Repository
+
+newtype NoOpRepository m a = NoOpRepository
+  { runNoOpRepository :: m a
+  }
+  deriving newtype (Functor, Applicative, Monad)
+  deriving MonadTrans via IdentityT
+
+instance Monad m => Repository (NoOpRepository m) where
+  save _ = pure ()
+  deleteAll = pure ()
+
+----- program
+
+program ::
+     Monad m
+  => Time m
+  => Queue m
+  => Repository m
+  => m Bool
+program = do
+  time <- currentTime
+  send "value"
+  save "value"
+  pure True
 
 tests :: IO Bool
-tests =
-  checkParallel $$(discover)
+tests = program
+  & runNoOpQueue
+  & runConstTime 10
+  & runNoOpRepository
