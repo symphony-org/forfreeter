e14704dcb40b15172b8be4add65e7d8235246333
Pawel Szulc <paul.szulc@gmail.com>
2020-10-15 18:18:11 +0200

    Add support for single monomorphic parameter in overlappable instance (#10)
    * Add support for single monomorphic parameter in overlappable instance
    
    Co-authored-by: Dominik Zajkowski <dzajkowski@invisibl.es>
    
    * Examples in comments
    
    Co-authored-by: Dominik Zajkowski <dzajkowski@invisibl.es>
    Co-authored-by: Łukasz Gołębiewski <lukasz.golebiewski@gmail.com>

--- src/Forfreeter/Instance.hs
+++ src/Forfreeter/Instance.hs
@@ -24,14 +24,23 @@ mkOverlappable cName = do
   (ClassI (ClassD _cxt _name _tyVarBndr _funDep decs) _knownInstances) <- reify cName
   let
     liftDecl :: Dec -> Q [Dec]
-    liftDecl (SigD name appT@(AppT _ _)) = do
-      let
-        body = NormalB $ AppE (VarE 'lift) (VarE name)
-      pure [ValD (VarP name) body []]
+    -- function with single polymorphic parameter, e.g. foo :: a -> m ()
     liftDecl (SigD name (ForallT [KindedTV paramName _] _ _)) = do
       let
         body = (NormalB $ AppE (VarE 'lift) (AppE (VarE name) (VarE paramName)))
       pure [FunD name [Clause [VarP paramName] body []]]
+    -- function with single monomorphic parameter, e.g. foo :: String -> m ()
+    liftDecl (SigD name (AppT (AppT ArrowT _) (AppT _ _))) = do
+      paramName <- newName "p1"
+      let
+        body = NormalB (InfixE (Just (VarE 'lift)) (VarE '($)) (Just (AppE (VarE name) (VarE paramName))))
+      pure [FunD name [Clause [VarP paramName] body []]]
+    -- function without parameters
+    liftDecl (SigD name appT@(AppT _ _)) = do
+      let
+        body = NormalB $ AppE (VarE 'lift) (VarE name)
+      pure [ValD (VarP name) body []]
+    -- ignore everything else
     liftDecl _                  = pure []
   functionDeclarations <- fmap join (traverse liftDecl decs)
   pure [InstanceD overlappable ctx (AppT (ConT cName) instanceType) functionDeclarations]
